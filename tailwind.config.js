2141d4cb02abab0d942918f8e67f5d51191f15f3
unknown <sawroarhossain5@gmail.com>
2024-11-30 16:26:16 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind intelisence
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
